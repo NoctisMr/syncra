0a08f127517bb55e1d1b83909abd363a3798489b
NoctisMr <noxis.mr@gmail.com>
2026-07-01 08:17:36 -0300

    Refactor main.dart for clarity and organization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// Archivo: lib/main.dart
+// lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -11,19 +11,15 @@ import 'features/shipping/presentation/providers/shipping_provider.dart';
 import 'features/navigation/main_navigation_wrapper.dart';
 
 void main() async {
-  // 1. Asegurar la inicialización de los canales nativos
   WidgetsFlutterBinding.ensureInitialized();
   
-  // 2. Bloquear en modo vertical
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
 
-  // 3. Inicializa la base de datos local (Hive)
   await LocalStorageService.instance.init();
 
-  // 4. Iniciar la app
   runApp(const MyApp());
 }
 
@@ -39,28 +35,22 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => ConverterProvider()),
         ChangeNotifierProvider(create: (_) => ShippingProvider()),
       ],
-      // 🌟 SOLUCIÓN: Usamos Consumer para escuchar dinámicamente los cambios visuales y refrescar el tema
       child: Consumer<AppProvider>(
         builder: (context, appProvider, child) {
           return MaterialApp(
             title: 'Global Wallet & Shipping',
             debugShowCheckedModeBanner: false,
-            themeMode: appProvider.themeMode, // Vinculado a las opciones guardadas
-            
-            // TEMA CLARO DINÁMICO
+            themeMode: appProvider.themeMode,
             theme: ThemeData(
               useMaterial3: true,
               colorScheme: ColorScheme.fromSeed(
-                seedColor: appProvider.seedColor, // Color puro o inteligentemente mezclado con la foto
+                seedColor: appProvider.seedColor,
                 brightness: Brightness.light,
               ),
-              // Si hay imagen de fondo, hacemos el andamiaje transparente para renderizar el fondo real detrás
               scaffoldBackgroundColor: appProvider.backgroundImagePath != null 
                   ? Colors.transparent 
                   : null,
             ),
-            
-            // TEMA OSCURO DINÁMICO
             darkTheme: ThemeData(
               useMaterial3: true,
               colorScheme: ColorScheme.fromSeed(
@@ -71,8 +61,6 @@ class MyApp extends StatelessWidget {
                   ? Colors.transparent 
                   : null,
             ),
-            
-            // Espera a que termine la carga asíncrona de IPs, APIs e idiomas antes de renderizar la UI
             home: appProvider.isLoading 
                 ? const Scaffold(body: Center(child: CircularProgressIndicator()))
                 : const MainNavigationWrapper(),
